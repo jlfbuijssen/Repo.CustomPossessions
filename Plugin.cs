19f5a23a8a04987253b75efc9c877eddb8afdde6
Jeffrey <jeffrey.buijssen@breenservices.nl>
2025-06-18 14:05:23 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace alt.dev.plugins.CustomPosessions;
+
+[BepInPlugin("dev.AltCtrler.plugin.CustomPossesion", "Alt's custom possesions", pluginVersion)]
+public class Plugin : BaseUnityPlugin
+{
+    private const string pluginGUID = "dev.AltCtrler.plugin.CustomPossesion";
+    private const string pluginName = "Alt's custom possesions";
+    private const string pluginVersion = "0.0.0.1";
+
+
+    //internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        //Logger = base.Logger;
+        //Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
